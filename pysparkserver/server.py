6b00ae9945d2d47b06ae6565a3747ad486d2696f
WANGJINGHE <wangjinghe258@gmail.com>
2017-11-26 20:14:19 +0800

    pysaprkserver final

--- pysparkserver/server.py
+++ pysparkserver/server.py
@@ -1,31 +1,58 @@
-# from pyspark import SparkContext
-# from pyspark.sql import SparkSession
-# from pyspark.sql.functions import *
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 import socket
 import threading
 import time
 import json
-import test_prediction
+import numpy as np
+from collections import OrderedDict
+sc=SparkContext('local')
 
-# sc=SparkContext('local')
-
-# spark = SparkSession \
-#     .builder \
-#     .appName('name') \
-#     .config("spark.sql.shuffle.partitions",5) \
-#     .getOrCreate()
+spark = SparkSession \
+    .builder \
+    .appName('name') \
+    .config("spark.sql.shuffle.partitions",5) \
+    .getOrCreate()
 
 
-# def reader(dir):
-#     df=spark.read.csv(dir)
+def reader(dir):
+    df=spark.read.csv(dir)
 
-#     col=list(df.toPandas.iloc[:3,0])
-#     return col
+    col=list(df.toPandas.iloc[:3,0])
+    return col
 
 
 
 def list2str(ls):
   pass
+
+
+def dict2list(dic:dict):
+
+    keys = dic.keys()
+    vals = dic.values()
+    lst = [(key, val) for key, val in zip(keys, vals)]
+    return lst
+
+
+
+
+def dict_rank(d):
+   return sorted(dict2list(d), key=lambda x: x[1], reverse=False)
+
+
+
+def tuple2json(tp):
+    dic=OrderedDict()
+    for t in tp:
+        dic[t[0]]=t[1]
+    return json.dumps(dic)
+
+
+
+
+
 
 
 
@@ -40,9 +67,9 @@ def parse_and_do(line):
     h=time.split(':')[0]
     min=time.split(':')[1]
     origin=string['origin']
-    print(origin+','+h+','+min)
-    back=test_prediction.prediction(origin+','+h+','+min)
-    # back=test_prediction.ma()
+    back=prediction(origin+','+h+','+min)
+    back=dict_rank(back)
+    back=tuple2json(back)
 
     if method=='get':
           return back
@@ -80,7 +107,7 @@ if __name__=='__main__':
 
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('10.0.0.5', 9999))
+    s.bind(('127.0.0.1', 9999))
     s.listen(5)
     print('waiting for connections')
     while True:
@@ -89,3 +116,4 @@ if __name__=='__main__':
 
       t = threading.Thread(target=tcplink, args=(sock, addr))
       t.start()
+
